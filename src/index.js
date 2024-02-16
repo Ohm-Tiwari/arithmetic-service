8b8480da7dddd3da98807f60a16bdc0107c56da3
Ohm-Tiwari <otiwari24@gmail.com>
2024-02-15 19:22:53 -0500

    adding more stuff

--- src/index.js
+++ src/index.js
@@ -7,26 +7,15 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Arithmetic service - Hello World!');
 });
-app.listen(port);
-
 app.get('/add/:n/:m', (req, res) => {
-  res.json(Number(req.params.n) + Number(req.params.m))
-});
-
-
-
-app.get('/sum', (req, res) => {
-    const num1 = parseInt(req.query.num1);
-    const num2 = parseInt(req.query.num2);
+    const n = Number(req.params.n);
+    const m = Number(req.params.m);
   
-    if (isNaN(num1) || isNaN(num2)) {
-      res.status(400).send('Invalid numbers');
-      return;
-    }
-  
-    const sum = num1 + num2;
-    res.send(`The sum is: ${sum}`);
+    let sum = add(n, m);
+      res.json(sum);
   });
+
+  app.listen(port);
 
 
 
