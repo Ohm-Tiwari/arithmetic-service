be730c9c5d5d721f093dfff58ffd36e3495770ba
Ohm-Tiwari <otiwari24@gmail.com>
2024-03-05 15:11:45 -0500

    made changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-
+require('dotenv').config()
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { add } = require("./arithmetica");
 const app = express();
-const port = 3000;
+app.use(cors());
 
+if (!process.env.PORT) {
+    throw new Error(`Please specify the
+    port number for the HTTP server with the environment variable PORT. `);
+    }
+
+const port = process.env.PORT;
 app.get('/', (req, res) => {
-    res.send('Arithmetic service - Hello World!');
+    res.send('Arithmetic service - last updated 3/5/2024');
 });
 app.get('/add/:n/:m', (req, res) => {
     const n = Number(req.params.n);
-    const m = Number(req.params.m);
-  
+    const m = Number(req.params.m); 
     let sum = add(n, m);
       res.json(sum);
   });
-
-  
   app.listen(port);
 
 
